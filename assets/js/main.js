6f4821b9e9c4ef0719a07e4d51be9aea21c050e1
blattinum <collin.blatt@gmail.com>
2021-02-28 04:57:55 -0500

    ...

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,14 @@
 function loadXMLDoc() {
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        myFunction(this);
-      }
-    };
-    xmlhttp.open("GET", "https://www.nationstates.net/cgi-bin/api.cgi?region=united_alliances&q=numnations", true);
-    xmlhttp.send();
-  }
+  fetch("https://www.nationstates.net/cgi-bin/api.cgi?region=united_alliances")
+  .then(response => response.text())
+  .then(data => {
+    let parser = new DOMParser();
+    let xml = parser.parseFromString(data, "application/xml");
+
+    console.log(xml);
   
-  function myFunction(xml) {
-    var x, i, xmlDoc, txt;
-    xmlDoc = xml.responseXML;
-    txt = "";
-    x = xmlDoc.getElementsByTagName("NUMNATIONS");
-    for (i = 0; i< x.length; i++) {
-      txt += x[i].childNodes[0].nodeValue + "<br>";
-    }
-    document.querySelector(".numnations").innerHTML = txt;
+    document.querySelector('.numnations').innerHTML = xml.documentElement.childNodes[5].innerHTML;
+
+  })
+  .catch(console.error);
   }
